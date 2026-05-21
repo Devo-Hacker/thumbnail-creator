1328dae1d85087b3c013d155d75a919aeb59feac
Devo-hacker <niladreebiharinayak@gmail.com>
2026-05-21 17:28:09 +0530

    including new things and updating generate UI

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,6 +1,62 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom"
+import type { IThumbnail } from "../assets/assets";
+import SoftBackDrop from "../components/SoftBackDrop";
+import { button } from "motion/react-client";
+
 const generate = () => {
+
+    const {id} = useParams();
+    const [title, setTitle] = useState('');
+    const [additionalDetails, setAdditionalDetails] = useState('');
+    const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null);
+    const [loading, setLoading] = useState(false);
   return (
-    <div>generate</div>
+    <>
+      <SoftBackDrop/>
+      <div className="pt-24 min-h-screen">
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 1g:px-8 py-8 pb-28 1g:pb-8">
+          <div className="grid lg:grid-cols-[400px_1fr] gap-8">
+
+            {/* left pannel */}
+            <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
+              <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+                <div>
+                  <h2 className="text-xl font-bold text-zinc-100 mb-1">Create your Thumbnail</h2>
+                  <p className="text-sm text-zinc-400">Show you creativity...</p>
+                </div>
+
+                <div className="space-y-5">
+                <div className="space-y-2">
+                  <label className="block text-sm font-medium">Title:</label>
+                  <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} maxLength={100} placeholder="Eg. How to brainrot?" className="
+                  w-full px-4 py-3 rounded-lg border border-white /12 bg-black/20 text-zinc-100 placeholder:text-zinc-400 focus:outline-none
+                  focus:ring-2 focus:ring-pink-500"/>
+                  <div className="flex justify-end">
+                    <span className="text-xs text-zinc-400">{title.length}/100</span>
+                  </div>
+                  </div>
+                    {/* aspect ratio selector */}
+                    <div className="space-y-2">
+                      <label className="block text-sm font-medium">Additional Prompts
+                        <span className="text-zinc-400 text-xs">(Optional)</span>
+                      </label>
+                    </div>
+                </div>
+
+                {!id && (
+                  <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-cyan-600 to-indigo-600 hover:from-cyan-700 disabled: cursor-not-allowed transition-colors">
+                    {loading ? 'Generating....' : 'Generate Thumbnail'}
+                  </button>
+                )}
+              </div>
+            </div>
+
+            {/* right pannel */}
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
